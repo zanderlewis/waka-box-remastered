c67a453f5d4e8f952f9c0f1f150da1f70bf704ed
Zander Lewis <zander@zanderlewis.dev>
2024-10-04 17:21:42 -0400

    Revert "Update index.js"
    This reverts commit e62ad31a125402273848017664b40e118f130c73.

--- index.js
+++ index.js
@@ -9,61 +9,74 @@ const {
 } = process.env;
 
 const wakatime = new WakaTimeClient(wakatimeApiKey);
+
 const octokit = new Octokit({ auth: `token ${githubToken}` });
 
 async function main() {
-  try {
-    const stats = await wakatime.getMyStats({ range: RANGE.LAST_30_DAYS });
-    await updateGist(stats);
-  } catch (error) {
-    console.error(`Error fetching WakaTime stats: ${error}`);
-  }
+  const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
+  await updateGist(stats);
 }
 
 function trimRightStr(str, len) {
+  // Ellipsis takes 3 positions, so the index of substring is 0 to total length - 3.
   return str.length > len ? str.substring(0, len - 3) + "..." : str;
 }
 
 async function updateGist(stats) {
+  let gist;
   try {
-    const gist = await octokit.gists.get({ gist_id: gistId });
-    const lines = generateGistContent(stats);
-
-    if (lines.length > 0) {
-      const filename = Object.keys(gist.data.files)[0];
-      await octokit.gists.update({
-        gist_id: gistId,
-        files: {
-          [filename]: {
-            filename: `📊 Weekly development breakdown`,
-            content: lines.join("\n")
-          }
-        }
-      });
-    }
+    gist = await octokit.gists.get({ gist_id: gistId });
   } catch (error) {
-    console.error(`Unable to update gist: ${error}`);
+    console.error(`Unable to get gist\n${error}`);
   }
-}
 
-function generateGistContent(stats) {
-  return stats.data.languages.slice(0, 5).map(data => {
+  const lines = [];
+  for (let i = 0; i < Math.min(stats.data.languages.length, 5); i++) {
+    const data = stats.data.languages[i];
     const { name, percent, text: time } = data;
 
-    return [
+    const line = [
       trimRightStr(name, 10).padEnd(10),
       time.padEnd(14),
       generateBarChart(percent, 21),
-      `${percent.toFixed(1).padStart(5)}%`
-    ].join(" ");
-  });
+      String(percent.toFixed(1)).padStart(5) + "%"
+    ];
+
+    lines.push(line.join(" "));
+  }
+
+  if (lines.length == 0) return;
+
+  try {
+    // Get original filename to update that same file
+    const filename = Object.keys(gist.data.files)[0];
+    await octokit.gists.update({
+      gist_id: gistId,
+      files: {
+        [filename]: {
+          filename: `📊 Weekly development breakdown`,
+          content: lines.join("\n")
+        }
+      }
+    });
+  } catch (error) {
+    console.error(`Unable to update gist\n${error}`);
+  }
 }
 
 function generateBarChart(percent, size) {
-  const filledSize = Math.round((percent / 100) * size);
-  const unfilledSize = size - filledSize;
+  const syms = "░▏▎▍▌▋▊▉█";
 
-  return "=".repeat(filledSize) + "_".repeat(unfilledSize);
+  const frac = Math.floor((size * 8 * percent) / 100);
+  const barsFull = Math.floor(frac / 8);
+  if (barsFull >= size) {
+    return syms.substring(8, 9).repeat(size);
+  }
+  const semi = frac % 8;
+
+  return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
+    .join("")
+    .padEnd(size, syms.substring(0, 1));
 }
 
 (async () => {
